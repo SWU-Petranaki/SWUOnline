6b22f02f0e56b8c645e492e3d5c86a94293eaafd
Bruno Braz <bbrazsilveira@gmail.com>
2025-04-02 20:49:43 -0300

    Revert "fix(CheckUniqueCard): correct unique upgrades check"

--- AllyAbilities.php
+++ AllyAbilities.php
@@ -133,8 +133,9 @@ function CheckUniqueCard($cardID, $allyUniqueID, $reportMode = false) {
 
     for ($i = 0; $i < count($allies); $i += AllyPieces()) {
       $otherAlly = new Ally("MYALLY-" . $i, $p);
+      if ($otherAlly->UniqueID() == $allyUniqueID) continue;
 
-      if ($otherAlly->CardID() == $cardID && !$otherAlly->IsCloned() && $otherAlly->Controller() == $player && $otherAlly->UniqueID() != $allyUniqueID) {
+      if ($otherAlly->CardID() == $cardID && !$otherAlly->IsCloned() && $otherAlly->Controller() == $player) {
         $uniqueAllyInPlay = $otherAlly;
         break;
       } else { //check for upgrades/pilots
