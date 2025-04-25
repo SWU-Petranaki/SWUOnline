fe625d5dd4c876047306fa138de8e57f75e5c2b2
LanceVE <159489051+LanceVE@users.noreply.github.com>
2025-04-24 22:56:49 -0500

    Card Back + Fix Concat

--- Libraries/NetworkingLibraries.php
+++ Libraries/NetworkingLibraries.php
@@ -558,7 +558,14 @@ function ProcessInput($playerID, $mode, $buttonInput, $cardID, $chkCount, $chkIn
       WriteLog("Player " . $playerID . " cancel their blocks.");
       break;
     case 10003: //Revert to prior turn
+      $undoCount = intval(GetCachePiece($gameName, ($playerID == 1 ? 27 : 28)));
+      WriteLog($undoCount);
+      if ($undoCount >= 10) {
+        WriteLog("Player $playerID has exceeded the maximum number of undos per round.");
+        break;
+      }
       RevertGamestate($buttonInput);
+      IncrementCachePiece($gameName, ($playerID == 1 ? 27 : 28));
       WriteLog("Player " . $playerID . " reverted back to a prior turn.");
       break;
     case 10005:
