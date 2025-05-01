2b7ba5aadfc716967dc09b3e95a883f0cfb2a0be
Mario Torres <mariotorresdev@gmail.com>
2025-05-01 08:29:13 -0500

    fix: added more overrides

--- CardOverrides.php
+++ CardOverrides.php
@@ -41,7 +41,7 @@ function CardIDOverride($cardID) {
     case "GG_001": return "SOR_021"; //Dagobah Swamp
     case "GG_002": return "SOR_024"; //Echo Base
     case "GG_003": return "SOR_026"; //Catacombs of Cadera
-    case "GG_004": return "SOR_026"; //Jabba's Palace
+    case "GG_004": return "SHD_026"; //Jabba's Palace
     case "GG_005": return "SOR_001"; //Experience (Token Upgrade)
     case "GG_006": return "SOR_002"; //Shield (Token Upgrade)
     case "JTL_258": return "SOR_250"; //Corellian Freighter
@@ -56,54 +56,54 @@ function CardIDOverride($cardID) {
     case "JTLW_005": return "JTL_097"; //Leia Organa
     case "JTLW_006": return "JTL_100"; //Poe Dameron
     case "JTLW_007": return "JTL_103"; //Chewbacca
-    case "JTLW_008": return "7901484215"; //General Hux
-    case "JTLW_009": return "8437892403"; //IG-88
-    case "JTLW_010": return "2810644441"; //BB-8
-    case "JTLW_011": return "6864010809"; //Red Squadron Y-Wing
-    case "JTLW_012": return "0256547071"; //Red Five
-    case "JTLW_013": return "3248826052"; //Captain Tarkin
-    case "JTLW_014": return "8666297378"; //Shoot Down
-    case "JTLW_015": return "1919456338"; //Boba Fett
-    case "JTLW_016": return "2757811795"; //Anakin Skywalker
-    case "JTLW_017": return "8384504746"; //Han Solo
-    case "JTLW_018": return "6489060437"; //Fett's Firespray
-    case "JTLW_019": return "0417314165"; //Millennium Falcon
-    case "JTLW_020": return "5191307053"; //Swarming Vulture Droid
+    case "JTLW_008": return "JTL_134"; //General Hux
+    case "JTLW_009": return "JTL_141"; //IG-88
+    case "JTLW_010": return "JTL_145"; //BB-8
+    case "JTLW_011": return "JTL_051"; //Red Squadron Y-Wing
+    case "JTLW_012": return "JTL_151"; //Red Five
+    case "JTLW_013": return "JTL_161"; //Captain Tarkin
+    case "JTLW_014": return "JTL_176"; //Shoot Down
+    case "JTLW_015": return "JTL_189"; //Boba Fett
+    case "JTLW_016": return "JTL_197"; //Anakin Skywalker
+    case "JTLW_017": return "JTL_203"; //Han Solo
+    case "JTLW_018": return "JTL_240"; //Fett's Firespray
+    case "JTLW_019": return "JTL_249"; //Millennium Falcon
+    case "JTLW_020": return "JTL_256"; //Swarming Vulture Droid
     case "P25_001":
-    case "P25_002": return "4299138542"; //Bossk JTL
+    case "P25_002": return "JTL_187"; //Bossk
     case "P25_003":
     case "P25_004":
     case "P25_005":
-    case "P25_006": return "4966205041"; //Devastator JTL
+    case "P25_006": return "JTL_143"; //Devastator
     case "P25_007":
-    case "P25_008": return "1715486607"; //Black One JTL
+    case "P25_008": return "JTL_147"; //Black One
     case "P25_009":
     case "P25_010":
     case "P25_011":
-    case "P25_012": return "7223762901"; //Home One JTL
-    case "J25_001": return "5975430516"; //The Ghost
-    case "J25_002": return "7713416118"; //Ki-Adi-Mundi
-    case "J25_003": return "8562538488"; //Poggle The Lesser
-    case "J25_004": return "JTL_094"; //Luke Skywalker JTL
-    case "J25_005": return "4737300535"; //Darth Maul
-    case "J25_006": return "0428448817"; //Darth Vader JTL
-    case "J25_007": return "8776875806"; //Count Dooku TWI
-    case "J25_008": return "2412836213"; //Ahsoka Tano
-    case "J25_009": return "9092942444"; //Enfys Nest
+    case "P25_012": return "JTL_204"; //Home One
+    case "J25_001": return "JTL_053"; //The Ghost
+    case "J25_002": return "TWI_064"; //Ki-Adi-Mundi
+    case "J25_003": return "TWI_080"; //Poggle The Lesser
+    case "J25_004": return "JTL_094"; //Luke Skywalker
+    case "J25_005": return "TWI_135"; //Darth Maul
+    case "J25_006": return "JTL_142"; //Darth Vader
+    case "J25_007": return "TWI_138"; //Count Dooku
+    case "J25_008": return "TWI_194"; //Ahsoka Tano
+    case "J25_009": return "TWI_198"; //Enfys Nest
     case "P25_046": return "SHD_141"; //Kylo Ren
-    case "P25_044": return "4246161751"; //Obi-Wan Kenobi SOR
-    case "P25_039": return "5712012074"; //Admiral Ackbar
-    case "P25_040": return "4965992740"; //Agent Kallus
-    case "P25_041": return "4015981529"; //Red Three
+    case "P25_044": return "SOR_049"; //Obi-Wan Kenobi
+    case "P25_039": return "SOR_097"; //Admiral Ackbar
+    case "P25_040": return "SOR_115"; //Agent Kallus
+    case "P25_041": return "SOR_144"; //Red Three
     case "P25_042": return "SOR_196"; //Chewbacca
-    case "P25_043": return "1367697710"; //Han Solo SOR
-    case "P25_052": return "6818212006"; //Mandalorian Armor
-    case "P25_053": return "8558118489"; //Count Dooku TWI
-    case "P25_054": return "4728275106"; //Kit Fisto
-    case "P25_055": return "3824479984"; //Fett's Firespray SOR
-    case "P25_056": return "0958864493"; //Unnatural Life
-    case "P25_057": return "9776208290"; //Emperor Palpatine (Master of the Dark Side)
-    case "P25_059": return "6814503441"; //Rey
+    case "P25_043": return "SOR_198"; //Han Solo
+    case "P25_052": return "SHD_073"; //Mandalorian Armor
+    case "P25_053": return "TWI_138"; //Count Dooku
+    case "P25_054": return "TWI_165"; //Kit Fisto
+    case "P25_055": return "SOR_184"; //Fett's Firespray
+    case "P25_056": return "TWI_189"; //Unnatural Life
+    case "P25_057": return "SOR_135"; //Emperor Palpatine (Master of the Dark Side)
+    case "P25_059": return "SHD_046"; //Rey
     default: return $cardID;
   }
 }
