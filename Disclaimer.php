7639e20a4992e84994a6aae041ec55e9b0a5bc7e
OotTheMonk <nathanandrecoder@gmail.com>
2025-05-03 13:39:31 -0500

    Reduce disclaimer font size

--- Disclaimer.php
+++ Disclaimer.php
@@ -1,3 +1,3 @@
 <div class="disclaimer bg-blur">
-  <p>Petranaki is in no way affiliated with Disney or Fantasy Flight Games. Star Wars characters, cards, logos, and art are property of Disney and/or Fantasy Flight Games. This is a free educational tool for researching decks and strategies for in-person play. It does not include automated tournaments or rankings. All features are accessible without payment and are not intended for commercial use.</p>
+  <p>Petranaki is in no way affiliated with Disney or Fantasy Flight Games. Star Wars characters, cards, logos, and art are property of Disney and/or Fantasy Flight Games. This is a free educational tool for researching decks and strategies for in-person play. All features are accessible without payment and are not intended for commercial use.</p>
 </div>
