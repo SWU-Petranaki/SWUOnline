d1dfb1bae69497f01246568a407fba0b2203b3b7
Mario Torres <mariotorresdev@gmail.com>
2025-05-06 12:48:16 -0500

    feat(LOF): added Terentatek

--- GeneratedCode/GeneratedCardDictionaries.php
+++ GeneratedCode/GeneratedCardDictionaries.php
@@ -15473,7 +15473,6 @@ function IsUnimplemented($cardID) {
   $unimplementedCards = array (
   5482818255 => true,
   7691597101 => true,
-  8660042329 => true,
   9566815036 => true,
 );
   return isset($unimplementedCards[$cardID]);
