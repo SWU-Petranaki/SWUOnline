19137c1fbf09c2a2a57fe35bca22cea9c89589f0
Mario Torres <mariotorresdev@gmail.com>
2025-06-06 22:52:52 -0500

    feat(LOF): added Consumed by the Dark Side

--- CoreLogic.php
+++ CoreLogic.php
@@ -7616,11 +7616,20 @@ function PlayAbility($cardID, $from, $resourcesPaid, $target = "-", $additionalC
         AddDecisionQueue("MZOP", $currentPlayer, "ADDEXPERIENCE", 1);
       }
       break;
-    case "8580514429":
+    case "8580514429"://Pillio Star Compass
       //When Played: Search the top 3 cards of your deck for a unit, reveal it, and draw it.
       AddDecisionQueue("SEARCHDECKTOPX", $currentPlayer, "3;1;include-definedType-Unit");
       AddDecisionQueue("ADDHAND", $currentPlayer, "-", 1);
       AddDecisionQueue("REVEALCARDS", $currentPlayer, "-", 1);
+      break;
+    case "8621390428"://Consumed by the Dark Side
+      //Give 2 Experience tokens to a unit, then deal 2 damage to it.
+      AddDecisionQueue("MULTIZONEINDICES", $currentPlayer, "MYALLY&THEIRALLY");
+      AddDecisionQueue("SETDQCONTEXT", $currentPlayer, "Choose a unit to give 2 experience tokens to");
+      AddDecisionQueue("CHOOSEMULTIZONE", $currentPlayer, "<-", 1);
+      AddDecisionQueue("MZOP", $currentPlayer, "ADDEXPERIENCE", 1);
+      AddDecisionQueue("MZOP", $currentPlayer, "ADDEXPERIENCE", 1);
+      AddDecisionQueue("MZOP", $currentPlayer, DealDamageBuilder(2, $currentPlayer), 1);
       break;
     //PlayAbility End
     default: break;
