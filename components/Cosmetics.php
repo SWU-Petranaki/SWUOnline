5a7222ded534933919cc58906641a0a19d9f18e8
LanceVE <159489051+LanceVE@users.noreply.github.com>
2025-06-22 13:24:27 -0500

    4 new cardbacks

--- components/Cosmetics.php
+++ components/Cosmetics.php
@@ -59,6 +59,11 @@ function CardbacksDropdowns($settings) {
  $rv .= CreateSelectOption($SET_Cardback . "-57", "Star Wars Dad (Family)", $SET_Cardback . "-" . $settings[$SET_Cardback]);
  $rv .= CreateSelectOption($SET_Cardback . "-58", "Unplayable", $SET_Cardback . "-" . $settings[$SET_Cardback]);
  $rv .= CreateSelectOption($SET_Cardback . "-59", "Wasatch Wookies", $SET_Cardback . "-" . $settings[$SET_Cardback]);
+ $rv .= CreateSelectOption($SET_Cardback . "-60", "Coastal CantinaBG", $SET_Cardback . "-" . $settings[$SET_Cardback]);
+ $rv .= CreateSelectOption($SET_Cardback . "-61", "MoTheMonster", $SET_Cardback . "-" . $settings[$SET_Cardback]);
+ $rv .= CreateSelectOption($SET_Cardback . "-62", "Coastal Cantina", $SET_Cardback . "-" . $settings[$SET_Cardback]);
+ $rv .= CreateSelectOption($SET_Cardback . "-63", "Coastal Cantina (Name)", $SET_Cardback . "-" . $settings[$SET_Cardback]);
+ $rv .= CreateSelectOption($SET_Cardback . "-64", "Top Cut Target", $SET_Cardback . "-" . $settings[$SET_Cardback]);
  //continue adding card backs here
   return $rv;
 }
