c4e1e49352cc145931a280bb1da8470a7e3ea21c
Mario Torres <mariotorresdev@gmail.com>
2025-07-27 19:51:29 -0500

    fix(LOF): yodas saber base choice

--- CoreLogic.php
+++ CoreLogic.php
@@ -7602,7 +7602,9 @@ function PlayAbility($cardID, $from, $resourcesPaid, $target = "-", $additionalC
     case "5800386133"://Yoda's Lightsaber
       if (HasTheForce($currentPlayer)) {
         DQAskToUseTheForce($currentPlayer);
-        AddDecisionQueue("PASSPARAMETER", $currentPlayer, "MYCHAR-0", 1);
+        AddDecisionQueue("PASSPARAMETER", $currentPlayer, "MYCHAR-0,THEIRCHAR-0", 1);
+        AddDecisionQueue("SETDQCONTEXT", $currentPlayer, "Choose a base to heal", 1);
+        AddDecisionQueue("CHOOSEMULTIZONE", $currentPlayer, "<-", 1);
         AddDecisionQueue("MZOP", $currentPlayer, "RESTORE,3", 1);
       }
       break;
